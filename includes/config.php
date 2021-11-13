7109053accd4a09e0a58d4f61aedf8365d86adb7
Alexandr <dapoy.katunfox@gmail.com>
2021-11-13 11:09:14 +0300

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SITE_NAME', 'Twitter');
+define('HOST', 'http://' . $_SERVER['HTTP_HOST'] . '/twitter');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'twitter');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
---
+++ user_posts.php
@@ -0,0 +1,14 @@
+<?php 
+include_once "includes/functions.php";
+
+$id = 0;
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $id = $_GET['id'];
+}
+$posts = get_posts($id);	
+
+include_once "includes/header.php";
+include_once "includes/tweet_form.php";
+include_once "includes/posts.php";
+include_once "includes/footer.php"; 
+
